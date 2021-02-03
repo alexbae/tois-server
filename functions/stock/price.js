9eba0dd8f8229b739154acb7f480387949843cea
JUN MAN BAE <junmanbae@JUNs-MacBook-Pro.local>
2021-02-02 23:33:57 -0800

    previous close

--- functions/stock/price.js
+++ functions/stock/price.js
@@ -20,25 +20,14 @@ price.post('/stock/price', cors(), async (req, res) => {
     const body = JSON.parse(JSON.stringify(req.body));
     const { ticker } = body;
     console.log("stocks-api.js 14 | body", body.ticker);
-    const today = new Date()
-    const currentDate = new Date(today)
-    let backDate = 0
-
-    currentDate.setDate(currentDate.getDate() - backDate)
-
-    const date = currentDate.toISOString().split('T')[0]
 
     const request = await fetch(
-        `https://api.polygon.io/v1/open-close/${ticker}/${date}?apiKey=${process.env.POLYGON_API_KEY}`
+        `https://api.polygon.io/v2/aggs/ticker/${ticker}/prev?unadjusted=true?apiKey=${process.env.POLYGON_API_KEY}`
     )
 
     const data = await request.json();
 
-    if (data.status === 'OK') {
-        res.json(data);
-    } else {
-        backDate + 1
-    }
+    res.json(data);
 });
 
 module.exports = price;
